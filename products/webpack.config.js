77564bd7d92259c35828d2cbfe748807d76b122a
Anthony <bird.anthony@gmail.com>
2020-12-07 23:37:14 +0200

    adding container micro frontend directory

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,6 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     devServer: {
-        port: 8080
-    }
+        port: 8081
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 };
