25761922454daf830d83f97ff5d256f5e0874b15
Anthony <bird.anthony@gmail.com>
2020-12-08 00:39:46 +0200

    adding some bootstrap setup

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -4,15 +4,14 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 module.exports = {
     mode: 'development',
     devServer: {
-        port: 8081
+        port: 8080
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'products',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './ProductsIndex': './src/index'
-            },
+            name: 'container',
+            remotes: {
+                products: 'products@http://localhost:8081/remoteEntry.js'
+            }
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
